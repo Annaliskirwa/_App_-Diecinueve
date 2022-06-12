3e5600e633699751f1bb305038541ca4388a1d2d
Annaliskirwa <annaliskirwa@gmail.com>
2022-06-12 20:33:43 +0300

    Configured log out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,6 @@ import { AuthService } from '@auth0/auth0-angular';
 export class AppComponent {
   title = 'kcb_customer_create';
 
-  constructor(public auth: AuthService){}
+  constructor(public auth: AuthService,
+    @Inject(DOCUMENT) public document: Document){}
 }
