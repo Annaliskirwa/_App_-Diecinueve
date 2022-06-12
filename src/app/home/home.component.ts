9ebefb7231bd7286fdd30df90b75f2ebab94006c
Annaliskirwa <annaliskirwa@gmail.com>
2022-06-12 23:58:03 +0300

    Added the browser animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { style, state, animate, transition, trigger, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [   // :enter is alias to 'void => *'
+        style({ opacity: 0 }),
+        animate(500, style({ opacity: 1 }))
+      ])
+    ]),
+    trigger('fadeInGrow', [
+      transition(':enter', [
+        query(':enter', [
+          style({ opacity: 0, marginTop: 30 }),
+          stagger('80ms', [
+            animate('400ms ease', style({ opacity: 1, marginTop: 0 }))
+          ])
+        ], { optional: true })
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
