5914c84bc376cafd5d14c9b6d94b55bdd4a2c37a
Annaliskirwa <annaliskirwa@gmail.com>
2022-06-12 23:04:59 +0300

    created the layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'kcb_customer_create';
