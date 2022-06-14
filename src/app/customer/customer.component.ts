6361fe1b4396b5064a8b226bac9269611c3b42b4
Annaliskirwa <annaliskirwa@gmail.com>
2022-06-14 09:06:13 +0300

    Added the animations property to customer

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,11 +1,31 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { style, state, animate, transition, trigger, query, stagger } from '@angular/animations';
+import { ApiService } from "../service/api.service";
 import { AuthService } from '@auth0/auth0-angular';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  styleUrls: ['./customer.component.css'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [   // :enter is alias to 'void => *'
+        style({ opacity: 0 }),
+        animate(500, style({ opacity: 1 }))
+      ])
+    ]),
+    trigger('fadeInGrow', [
+      transition(':enter', [
+        query(':enter', [
+          style({ opacity: 0, marginTop: 30 }),
+          stagger('80ms', [
+            animate('400ms ease', style({ opacity: 1, marginTop: 0 }))
+          ])
+        ], { optional: true })
+      ])
+    ])
+  ]
 })
 export class CustomerComponent implements OnInit {
 
